27d6d8ca7bb88e976d1f3852fae62d8a689e4609
ffrowies <fernandorowies.saxo@gmail.com>
2021-06-22 10:39:03 -0300

    Initial commit

---
+++ app/src/main/java/com/ffrowies/listdothat/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ffrowies.listdothat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ffrowies.listdothat.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
